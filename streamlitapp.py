29b707ab48a30b667e43cc9bbedf8d9a1e2ef5c0
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-03-31 21:33:27 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -9,3 +9,8 @@ overview = st.container()
 left, right = st.columns(2)
 prediction = st.container()
 
+sex_d = {0:"Female",1:"Male"}
+alc_d = {0:"No",1:"Yes"}
+race_d = {0:"White", 1:"Black", 2:"Asian", 3:"Hispanic", 4:"American Indian/Alaskan Native", 5: "Other"}
+
+
