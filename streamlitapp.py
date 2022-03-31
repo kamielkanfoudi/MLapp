0c5eed2111f068ee8563b6dbb7f09b1124b192eb
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-03-31 17:44:50 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,3 +3,8 @@ import pandas as pd
 import altair as alt
 
 st.title('Predicting Heat Disease with Ensemble Models')
+
+st.set_page_config(page_title="Titanic Survival App")
+overview = st.beta_container()
+left, right = st.beta_columns(2)
+prediction = st.beta_container()
