faf95d6905290da2911996db56bfaefe327ca8ec
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-04-10 14:30:22 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -5,12 +5,5 @@ import pickle
 
 
 st.set_page_config(page_title="Predicting Heat Disease with Ensemble Models")
-overview = st.container()
-left, right = st.columns(2)
-prediction = st.container()
-
-sex_d = {0:"Female",1:"Male"}
-alc_d = {0:"No",1:"Yes"}
-race_d = {0:"White", 1:"Black", 2:"Asian", 3:"Hispanic", 4:"American Indian/Alaskan Native", 5: "Other"}
 
 
