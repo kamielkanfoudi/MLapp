c2d632795341b8c4e225c99344691f72e9d02af6
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-04-10 17:42:54 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -12,7 +12,7 @@ prediction = st.container()
 
 with overview:
 	st.title("Predicting Heart Disease")
-	st.markdown("Predicting probability of getting heart disease")
+	st.markdown("Predicting probability of getting heart disease using a Random Forest Classifier and CDC data.")
 #filename = "RFC_model-sv.sv"
 filename = "PLG_model.sv"
 model = pickle.load(open(filename,'rb'))
