151318429d61037f4cf4a95c0ab143c99ac90a96
marlenelie <87947672+marlenelie@users.noreply.github.com>
2022-04-10 15:36:20 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -24,3 +24,11 @@ Diabetic = {0:"No",1:"Yes"}
 GenHealth = {0:"Poor", 1:"Fair", 2:"Good", 3:"Very good", 4:"Excellent"}
 AgeCategory = {0:"18-24", 1:"25-29", 2:"30-34", 3:"35-39", 4:"40-44", 5:"45-49", 6:"50-54", 7:"55-59", 8:"60-64", 9:"65-69", 10:"70-74", 11:"75-79", 12:"80 or older"}
 
+st.set_page_config(page_title="Predicting Heart Disease")
+overview = st.beta_container()
+left, right = st.beta_columns(2)
+prediction = st.beta_container()
+
+with overview:
+	st.title("Predicting Heart Disease")
+	st.markdown("Predicting probability of getting heart disease")
