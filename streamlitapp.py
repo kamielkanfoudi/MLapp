a6a2f1eccd2f2f0e11f3abd715d3d278ff989248
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-04-10 15:48:53 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -34,7 +34,7 @@ AgeCategory = {0:"18-24", 1:"25-29", 2:"30-34", 3:"35-39", 4:"40-44", 5:"45-49",
 with left:
 	Smoking_radio = st.radio( "Smoking", list(Smoking.keys()), format_func=lambda x : Smoking[x] )
 	AlcoholDrinking_radio = st.radio( "AlcoholDrinking", list(AlcoholDrinking.keys()), format_func=lambda x : AlcoholDrinking[x]
-	age_radio = st.radio( "Age", list(AgeCategory.keys()), format_func=lambda x : AgeCategory[x] )
+	Age_radio = st.radio( "Age", list(AgeCategory.keys()), format_func=lambda x : AgeCategory[x] )
 	GenHealth_radio = st.radio( "GenHealth", list(AgeCategory.keys()), format_func=lambda x : GenHealth[x] )
 	Diabetic_radio = st.radio( "Diabetic", list(Diabetic.keys()), format_func=lambda x : Diabetic[x] )
 	Race_radio = st.radio( "Race", list(Race.keys()), format_func=lambda x : Race[x] )
